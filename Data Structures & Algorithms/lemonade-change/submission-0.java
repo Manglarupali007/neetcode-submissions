753575f7ee3d180e58f7c659ab628123e230474c
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-08-24 19:26:19 +0530

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int n=bills.length;
+        int five=0;
+        int ten=0;
+        int twe=0;
+        for(int i=0;i<n;i++){
+            if(bills[i]==5) five++;
+            else if(bills[i]==10){
+                 ten++;
+                 if(five==0) return false;
+                else if(five>0) five--;
+            }
+            else {
+                twe++;
+                if(ten>0 && five>0){
+                     ten--;
+                    five--;
+                }
+                else if(five>=3) five-=3;
+                else return false;
+            }
+        }
+        return true;
+    }
+}
